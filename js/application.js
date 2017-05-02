25bed188a4e847e231ca69b09d7c6226cc2c8641
pongo-pygmaeus <maxwellmathews@gmail.com>
2017-05-02 09:06:11 -0700

    Update mapping of spin rotation

--- js/application.js
+++ js/application.js
@@ -2,43 +2,20 @@ $(document).ready(function(){
   var totalRotation = 0;
   var segments = 0;
   var place = 0;
-
-  var digit = function(angle) {
-    switch(angle) {
-        case 0:
-        return 1;
-        break;
-
-        case 60:
-        return 6;
-        break;
-
-        case 120:
-        return 5;
-        break;
-
-        case 180:
-        return 4;
-        break;
-
-        case 240:
-        return 3;
-        break;
-
-        case 300:
-        return 2;
-        break;
-
-        default: return 1; // exit this handler for other keys
-    }
-  }
+  var options = {};
+  options[0] = 1;
+  options[1] = 6;
+  options[2] = 5;
+  options[3] = 4;
+  options[4] = 3;
+  options[5] = 2;
 
   $("img").on("click", function(e) {
 
     segments = Math.floor(Math.random() * (50));
     totalRotation = 60 * segments;
-    place = segments % 6
-    console.log(digit(place * 60))
+    individualSegment = segments % 6
+    console.log(options[individualSegment])
 
     $("img").rotate({ animateTo: totalRotation })
     e.preventDefault();
