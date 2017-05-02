de653e25c65df650158cf58e5b9c518fc1cbf632
pongo-pygmaeus <maxwellmathews@gmail.com>
2017-05-01 17:18:32 -0700

    Adds cheesy mapping of rotation angle to spinner value for demo purposes. Need better math...

--- js/application.js
+++ js/application.js
@@ -1,8 +1,46 @@
 $(document).ready(function(){
-  var value = 0
+  var totalRotation = 0;
+  var segments = 0;
+  var place = 0;
+
+  var digit = function(angle) {
+    switch(angle) {
+        case 0:
+        return 1;
+        break;
+
+        case 60:
+        return 6;
+        break;
+
+        case 120:
+        return 5;
+        break;
+
+        case 180:
+        return 4;
+        break;
+
+        case 240:
+        return 3;
+        break;
+
+        case 300:
+        return 2;
+        break;
+
+        default: return 1; // exit this handler for other keys
+    }
+  }
+
   $("img").on("click", function(e) {
-    value = Math.floor(Math.random() * (3600 - 45)) + 45;
-    $("img").rotate({ animateTo: value })
+
+    segments = Math.floor(Math.random() * (50));
+    totalRotation = 60 * segments;
+    place = segments % 6
+    console.log(digit(place * 60))
+
+    $("img").rotate({ animateTo: totalRotation })
     e.preventDefault();
   })
 })
