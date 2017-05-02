cc41d0690cf87d59e63b5e4b205c2638d4c7ca4c
pongo-pygmaeus <maxwellmathews@gmail.com>
2017-05-02 09:55:14 -0700

    some class refactoring

--- js/application.js
+++ js/application.js
@@ -1,23 +1,42 @@
 $(document).ready(function(){
-  var totalRotation = 0;
-  var segments = 0;
-  var place = 0;
-  var options = {};
-  options[0] = 1;
-  options[1] = 6;
-  options[2] = 5;
-  options[3] = 4;
-  options[4] = 3;
-  options[5] = 2;
-
-  $("img").on("click", function(e) {
-
-    segments = Math.floor(Math.random() * (50));
-    totalRotation = 60 * segments;
-    individualSegment = segments % 6
-    console.log(options[individualSegment])
-
-    $("img").rotate({ animateTo: totalRotation })
-    e.preventDefault();
+  var spinner = new Spinner({
+    targetElement: "img",
+    segments:      6
   })
 })
+
+var Spinner = function(args) {
+  this.targetElement = args.targetElement;
+  this.segments      = args.segments || 6;
+  this.options       = {};
+
+  this.options[0] = "1: Chickens";
+  this.options[1] = "6: Cows";
+  this.options[2] = "5: Sheep";
+  this.options[3] = "4: Ducks";
+  this.options[4] = "3: Fish";
+  this.options[5] = "2: Pigs";
+
+  this.initializeSpinner();
+}
+
+Spinner.prototype.initializeSpinner = function() {
+  var MAX_ROTATIONS  = 50;
+  var spinner = this;
+  var degreesPerSegment = 360/spinner.segments;
+  var totalRotation = 0;
+  var turns = 0;
+  var place = 0;
+
+  $(spinner.targetElement).on("click", function(e) {
+
+    turns = Math.floor(Math.random() * (MAX_ROTATIONS));
+    totalRotation = degreesPerSegment * turns;
+    individualSegment = turns % spinner.segments;
+
+    console.log(spinner.options[individualSegment])
+
+    $("img").rotate({ animateTo: totalRotation })
+  })
+}
+
